8bcbc69b130d5fd562555843093da9a196f23b48
akniles <alexslinka98@gmail.com>
2020-06-09 15:16:49 +0300

    fixed I/O

--- examples/first.cpp
+++ examples/first.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../eintegrals.h"
+#include "../src/eintegrals.h"
 
 int main() {
 	std::cout << "Enter precision: \n";
@@ -12,9 +12,9 @@ int main() {
 	std::cout << "Enter eccentricity(0 < e < 1): \n";
 	std::cin >> eccentricity;
 	std::cout << "Value of complete elliptic integral of the first kind with given eccentricity is:\n"  
-			  << first_kind_integral(std::sqrt(2) / 2, precision) 
+			  << first_kind_integral(eccentricity, precision) 
 			  << "\nValue of complete elliptic integral of the second kind with given eccentricity is:\n" 
-			  << second_kind_integral(std::sqrt(2) / 2, precision)
+			  << second_kind_integral(eccentricity, precision)
 			  << "\n";
 	return 0;
 }
